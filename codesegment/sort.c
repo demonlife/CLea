c6ba18456efe9329547ec0d0bdc10fc957c6c8f5
demonlife <demonlife2013@gmail.com>
2014-06-01 21:28:45 +0800

    add some files

---
+++ codesegment/sort.c
@@ -0,0 +1,18 @@
+/*
+ * ≈≈–ÚÀ„∑®
+ *
+ */
+ 
+ // ≤Â»Î≈≈–Ú
+ void insert_sort(int *arr, int len) {
+	int i, j, tmp;
+	for (i = 1; i < len; ++i) {
+		tmp = arr[i];
+		j = i - 1;
+		while (j >= 0 && arr[j] > key) {
+			arr[j+1] = arr[j];
+			--j;
+		}
+		arr[j+1] = tmp;
+	}
+ }
