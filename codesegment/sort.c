0fdc5d38ea0b0624b84825535c53d242db021600
demonlife <demonlife2013@gmail.com>
2014-06-03 07:50:44 +0800

    modify some files

--- codesegment/sort.c
+++ codesegment/sort.c
@@ -16,3 +16,42 @@
 		arr[j+1] = tmp;
 	}
  }
+ 
+ // 归并排序
+ void merge(int *arr, int start, int mid, int end) {
+	int n1 = mid - start + 1;
+	int n2 = end - mid;
+	
+	int left[n1], right[n2]; // 能否想办法去掉
+	int i, j, k;
+	
+	for (i = 0; i < n1; ++i)
+		left[i] = arr[start+i];
+		
+	for (j = 0; j < n2; ++j)
+		right[j] = arr[mid+1+j];
+		
+	i = j = 0;
+	k = start;
+	while (i < n1 && j < n2) {
+		if (left[i] < right[j])
+			arr[k++] = left[i++];
+		else
+			arr[k++] = right[j++];
+	}
+	
+	while (i < n1)
+		arr[k++] = left[i++];
+		
+	while (j < n2)
+		arr[k++] = right[j++];
+}
+
+void sort(int *arr, int start, int end) {
+	if (start < end) {
+		int mid = (start + end) / 2;
+		sort(arr, start, mid);
+		sort(arr, mid+1, end);
+		merge(arr, start, mid, end);
+	}
+}	
