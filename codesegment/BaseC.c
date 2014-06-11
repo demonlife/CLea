3612378156568a5e4f31d100180e9c83cac93e99
demonlife2013@gmail.com <demonlife2013@gmail.com>
2014-06-11 13:55:14 +0800

    modify some files

--- codesegment/BaseC.c
+++ codesegment/BaseC.c
@@ -3,7 +3,8 @@
 #include <locale.h>
 #include <string.h>
 
-int main(void) {
+int main(int argc, char *argv[]) {
+#if 0
   setlocale(LC_CTYPE, "en_US.UTF-8"); // locale.h
   wchar_t wc = L'中'; // 宽字符
   char buf[100] = {};
@@ -16,8 +17,9 @@ int main(void) {
     printf("0x%02X ", (unsigned char)buf[i]);
   }
   printf("\n");
+#endif
 
-  #if 0
+#if 0
   wchar_t *ws = L"中国人";
   printf("%ls\n", ws);
 
@@ -28,9 +30,9 @@ int main(void) {
     printf("0x%02X ", (unsigned char)sbuf[i]);
   }
   printf("\n");
-  #endif
+#endif
 
-  #if 0
+#if 0
   wchar_t ws[] = L"中国人";
   printf("len %d, size %d\n", wcslen(ws), sizeof(ws));
   unsigned char *b = (unsigned char*)ws;
@@ -39,9 +41,9 @@ int main(void) {
   for (i = 0; i < len; ++i) {
     printf("%02X ", b[i]);
   }
-  #endif
+#endif
 
-  #if 0
+#if 0
   int *pi = &(int){ 123 };
   printf("%d\n", *pi);
   int *x = (int[]){1, 2, 3, 4};
@@ -79,6 +81,7 @@ int main(void) {
 #if 1
   // 字符串分割
   
+#if 0
   //strtok是不可重入的
   char str[] = "root:x::0:root:/root:/bin/bash:";
   char *token;
@@ -86,9 +89,33 @@ int main(void) {
   printf("token = %s \n", token);
   while ((token = strtok(NULL, ":")) != NULL)
     printf("%s\n", token);
+ #endif
   
   //strtok_r是可重入的
-  
+  char *str1, *str2, *token, *subtoken;
+  char *saveptr1, *saveptr2;
+  int j;
+
+  if (argc != 4) {
+    fprintf(stderr, "usage: %s string delim subdelim\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+
+  for (j = 1, str1 = argv[1]; ; ++j, str1 = NULL) {
+    token = strtok_r(str1, argv[2], &saveptr1);
+    printf("first token = %s\n", token);
+    if (token == NULL)
+      break;
+    printf("%d: %s\n", j, token);
+
+    for (str2 = token; ; str2 = NULL) {
+      subtoken = strtok_r(str2, argv[3], &saveptr2);
+      printf(" inner subtoken = %s\n", subtoken);
+      if (subtoken == NULL)
+        break;
+      printf(" --> %s\n", subtoken);
+    }
+  }
 #endif
   
   return 0;
