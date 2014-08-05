6b4420f58c29bf831208f72b0ac98cebd83a524d
demonlife <demonlife2013@gmail.com>
2014-08-05 09:13:45 +0800

    modify good_code.c

--- codesegment/good_code.c
+++ codesegment/good_code.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
 // 删除字符串中的某个字符
 void deleteonecharacterinstr(char *s, int c) {
@@ -19,9 +21,23 @@ int bitcount(int x) {
   return b;
 }
   
+// 删除字符串末尾的空格，制表符，与换行符
+int trim(char *str) {
+  int len;
+  for(len = strlen(str) - 1; len >= 0; --len)
+    if(str[len] != '\n' && str[len] != ' ' && str[len] != '\t')
+      break;
+  str[len+1] = '\0';
+  return len;
+}
+
 int main() {
-  char s[] = "abccdcdcd";
+  char s[] = "abccdcdcd   ";
   deleteonecharacterinstr(s, 'c');
   printf("%s\n", s);
+
+  char s2[] = "a bbbbbbb        s";
+  trim(s2);
+  printf("%send\n", s2);
   return 0;
 }
