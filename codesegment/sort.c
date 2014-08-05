f2935aca689d8b7f2c8ddad2801880e2108a390b
demonlife <demonlife2013@gmail.com>
2014-08-05 08:21:11 +0800

    modify sort.c

--- codesegment/sort.c
+++ codesegment/sort.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+
 /*
  * 排序算法
  *
@@ -9,7 +11,7 @@
 	for (i = 1; i < len; ++i) {
 		tmp = arr[i];
 		j = i - 1;
-		while (j >= 0 && arr[j] > key) {
+		while (j >= 0 && arr[j] > tmp) {
 			arr[j+1] = arr[j];
 			--j;
 		}
@@ -55,3 +57,27 @@ void sort(int *arr, int start, int end) {
 		merge(arr, start, mid, end);
 	}
 }	
+
+// shell 排序， 参见百度百科里的shell排序算法
+// http://baike.baidu.com/view/549624.htm
+void shellsort(int *arr, int n) {
+  int gap, i, j, tmp;
+  for(gap = n / 2; gap > 0; gap /= 2) {
+    for(i = gap; i < n; ++i) {
+      for(j = i - gap; j >= 0 && arr[j] > arr[j+gap]; j -= gap) {
+        tmp = arr[j];
+        arr[j] = arr[j+gap];
+        arr[j+gap] = tmp;
+      }
+    }
+  }
+}
+
+int main() {
+  int arr[] = {1, 3, 5, 7, 9, 2, 4, 6, 8, 10};
+  shellsort(arr, 10);
+  for(int i = 0; i < 10; ++i)
+    printf("%d ", arr[i]);
+  printf("\n");
+  return 0;
+}
